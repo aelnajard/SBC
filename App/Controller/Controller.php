75f630ae57a02b24c283d545255ade879f88db36
aelnajard <aelnajard@gmail.com>
2021-11-01 20:58:41 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -20,16 +20,19 @@ if ($_POST['module'] == 'setIp') {
  $userActions->setIp($geoplugin->ip);
  $countIp = $userActions->countUsersActionsByIp()["COUNT(*)"];
 
+ echo $countIp;
 
   if ($countIp > 0) {
     $userActions = new UsersActions(new Database());
     $userActions->setIp($geoplugin->ip);
     $idUser = $userActions->getIdUser();
 
-    $userActions = new UsersActions(new Database());
+    echo $idUser;
+
+  /*  $userActions = new UsersActions(new Database());
     $userActions->setIdUser($idUser);
     $userActions->setIp($geoplugin->ip);
-    $userActions->createUserActionIp();
+    $userActions->createUserActionIp(); */
 
   }
   else {
