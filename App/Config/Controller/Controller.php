1b1dc73531cdaf9e8208362c623209f4ef0a9922
aelnajard <aelnajard@gmail.com>
2021-12-26 16:17:11 -0500

    ...

--- App/Config/Controller/Controller.php
+++ App/Config/Controller/Controller.php
@@ -7,20 +7,11 @@ header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name
 require_once('../Lib/geoplugin.class.php');
 
 
-//whether ip is from share internet
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-  {
-    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-  }
-//whether ip is from proxy
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-  {
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  }
-//whether ip is from remote address
-else
-  {
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-  }
-echo $ip_address;
+
+
+$geoplugin = new geoPlugin();
+$geoplugin->locate();
+echo $geoplugin->ip;
+
+
  ?>
