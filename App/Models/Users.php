88b4a8d9ac3f6b1b5db4febc792d0ad54244a91e
aelnajard <aelnajard@gmail.com>
2021-12-28 17:09:14 -0500

    ...

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -18,9 +18,11 @@
         function createUser(){
 
           try{
-            $sql = "INSERT INTO `Users`(`NIT`, `email`)  VALUES (0,3)";
+            $sql = "INSERT INTO `Users`(`NIT`, `email`)  VALUES ('0','3')";
             $this->con->conn()->exec($sql);
-          //  $this->con->close();
+            $this->con->close();
+            return 1;
+
               }
           catch(PDOException $e){
               echo $query . "<br>" . $e->getMessage();
