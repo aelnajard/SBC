943463500df471a39977678350d20b43bd0f9d8f
aelnajard <aelnajard@gmail.com>
2021-12-28 13:08:46 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -4,16 +4,16 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
 
-require_once('../config/database.php');
+//require_once('../config/database.php');
 require_once('../Lib/geoplugin.class.php');
 
-private $userActions;
+//private $userActions;
 
-function queryUserActions(){
+/*function queryUserActions(){
   $db = new Database();
   $userActions = new UsersActions($db);
   $this->userActions = $userActions;
-}
+}*/
 
 if ($_POST['module'] == 'setIp') {
 
