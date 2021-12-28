94dfca116c9cfed0b37f3cb83a0468ee6f37c60d
aelnajard <aelnajard@gmail.com>
2021-12-28 13:44:37 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -8,14 +8,6 @@ require_once('../../App/Config/database.php');
 require_once('../../App/Lib/geoplugin.class.php');
 require_once('../../App/Models/UserActions.php');
 
-  $userActions;
-
-function queryUserActions(){
-  $db = new Database();
-  $userActions = new UsersActions($db);
-  //  $this->userActions = $userActions;
-}
-
 if ($_POST['module'] == 'setIp') {
 
  $geoplugin = new geoPlugin();
@@ -24,7 +16,8 @@ if ($_POST['module'] == 'setIp') {
   echo $geoplugin->ip;
 
 
-  queryUserActions();
+  $db = new Database();
+  $userActions = new UsersActions($db);
   //$this->userActions->setIp($geoplugin->ip);*/
   //$this->userActions->countUsersActionsIp()["COUNT(*)"];
 
