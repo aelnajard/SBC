0559fca7f267ad43ba0c7c182d766fda6e1af55d
aelnajard <aelnajard@gmail.com>
2021-12-28 13:19:24 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
 
-require_once('../Config/database.php');
+//require_once('../Config/database.php');
 //require_once('../Lib/geoplugin.class.php');
 echo "string";
 private $userActions;
