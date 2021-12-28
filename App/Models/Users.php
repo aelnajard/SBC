770ab008cb58682e6c51890141972129b304fca1
aelnajard <aelnajard@gmail.com>
2021-12-28 18:50:16 -0500

    ...

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -27,15 +27,15 @@
         }
 
         function getLastId(){
-        /* try{
-          $sql = $this->conn->conn()->query("SELECT `idUser` FROM `Users` ORDER BY `idUser`DESC LIMIT 1  ");
-            $data = $sql->fetch(PDO::FETCH_ASSOC);
-            $this->conn->close();
-            return $data;
+         try{
+        //    $sql = $this->conn->conn()->query("SELECT `idUser` FROM `Users` ORDER BY `idUser`DESC LIMIT 1  ");
+        //    $data = $sql->fetch(PDO::FETCH_ASSOC);
+        //    $this->conn->close();
+        //    return $data;
               }
           catch(PDOException $e){
               echo $query . "<br>" . $e->getMessage();
-            }*/
+            }
         }
 
 
