ed89ebe0c27a428d54465873b59f0b943b586b92
aelnajard <aelnajard@gmail.com>
2021-12-28 13:18:13 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -4,31 +4,31 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
 
-//require_once('../config/database.php');
-//require_once('../Lib/geoplugin.class.php');
+require_once('../Config/database.php');
+require_once('../Lib/geoplugin.class.php');
 echo "string";
-//private $userActions;
+private $userActions;
 
-/*function queryUserActions(){
+function queryUserActions(){
   $db = new Database();
   $userActions = new UsersActions($db);
   $this->userActions = $userActions;
-}*/
+}
 
 if ($_POST['module'] == 'setIp') {
 
-//  $geoplugin = new geoPlugin();
-//  $geoplugin->locate();
-//  $geoplugin->ip;
-//  echo $geoplugin->ip;
+  $geoplugin = new geoPlugin();
+  $geoplugin->locate();
+  $geoplugin->ip;
+  echo $geoplugin->ip;
 
-/*
+
   $readUsersActions = new Read();
   $readUsersActions->queryUserActions();
   $this->userActions->setIp($geoplugin->ip);*/
   //$this->userActions->countUsersActionsIp()["COUNT(*)"];
 
-
+/*
 /*  if ($countIp > 0) {
 
   }
