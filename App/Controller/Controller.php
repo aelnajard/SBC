6880215afd6a283f4a7c370d2447b6b146e00e02
aelnajard <aelnajard@gmail.com>
2021-12-28 13:19:06 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -5,19 +5,19 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
 
 require_once('../Config/database.php');
-require_once('../Lib/geoplugin.class.php');
+//require_once('../Lib/geoplugin.class.php');
 echo "string";
 private $userActions;
 
-function queryUserActions(){
+/*function queryUserActions(){
   $db = new Database();
   $userActions = new UsersActions($db);
   $this->userActions = $userActions;
-}
+}*/
 
 if ($_POST['module'] == 'setIp') {
 
-  $geoplugin = new geoPlugin();
+/*  $geoplugin = new geoPlugin();
   $geoplugin->locate();
   $geoplugin->ip;
   echo $geoplugin->ip;
@@ -25,7 +25,7 @@ if ($_POST['module'] == 'setIp') {
 
   $readUsersActions = new Read();
   $readUsersActions->queryUserActions();
-  $this->userActions->setIp($geoplugin->ip);
+  $this->userActions->setIp($geoplugin->ip);*/
   //$this->userActions->countUsersActionsIp()["COUNT(*)"];
 
 /*
