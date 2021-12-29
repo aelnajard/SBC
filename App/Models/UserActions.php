7c7a2fb795fa48acef36606bd27daec3e1d5de87
aelnajard <aelnajard@gmail.com>
2021-12-28 20:47:32 -0500

    ...

--- App/Models/UserActions.php
+++ App/Models/UserActions.php
@@ -42,10 +42,10 @@
 
       function getIdUser(){
         try{
-        /*   $sql = $this->conn->conn()->query("SELECT `idUser` FROM `UserActions` WHERE `ip` = '$this->ip' ORDER BY `idUserActions` DESC LIMIT 1");
+          $sql = $this->con->conn()->query("SELECT `idUser` FROM `UserActions` WHERE `ip` = '$this->ip' ORDER BY `idUserActions` DESC LIMIT 1");
            $data = $sql->fetch(PDO::FETCH_OBJ);
-           $this->conn->close();
-           return $data;*/
+           $this->con->close();
+           return $data;
            }
          catch(PDOException $e){
              echo $query . "<br>" . $e->getMessage();
