2bb2d04788ca40245f9ebb0a416c9de7098d6b7b
aelnajard <aelnajard@gmail.com>
2021-12-28 22:02:41 -0500

    ...

--- App/Models/UserActions.php
+++ App/Models/UserActions.php
@@ -66,9 +66,8 @@
 
 
       function createUserActionIp(){
-        echo $this->city."holalalala";
         try{
-          $sql = "INSERT INTO `UserActions`( `idUser`,  `ip`) VALUES ('$this->idUser',  '$this->ip')";
+          $sql = "INSERT INTO `UserActions`( `idUser`,  `ip`,`city`) VALUES ('$this->idUser',  '$this->ip','$this->city')";
           $this->con->conn()->exec($sql);
           $this->con->close();
             }
