7831c0cd4855d8924350aca1e2cef14ca60a1cd1
aelnajard <aelnajard@gmail.com>
2022-01-30 17:36:06 -0500

    ...

--- App/Models/Avatars.php
+++ App/Models/Avatars.php
@@ -18,7 +18,7 @@
 
         try{
 
-          $sql = $this->conn->conn()->query("SELECT `idAvatars` FROM  `Avatars` WHERE gender = 'male' AND name = '1'  ");
+          $sql = $this->conn->conn()->query("SELECT `idAvatars` FROM  `Avatars` WHERE gender = '$this->avatar' AND name = '1'  ");
             $data = $sql->fetchAll(PDO::FETCH_OBJ);
             $this->conn->close();
             return $data;
