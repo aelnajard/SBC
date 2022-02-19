c0132ab70851aa7b29cc7004162bd46b34b6da2e
aelnajard <aelnajard@gmail.com>
2022-02-19 15:23:11 -0500

    ...

--- Pages/Index/Turns/Turns.php
+++ Pages/Index/Turns/Turns.php
@@ -1,11 +1,49 @@
 <style media="screen">
   .turns{
+    position: relative;
     background-color: #0E7C7B;
+    padding: 40px;
+  }
+  .selectDay, .selecttime{
+    position: relative;
+    padding: 0px;
+    display: flex;
+    background-color: red;
+    justify-content: center;
+  }
+  .daysContainer{
+    height: 200px;
+    width: 200px;
+    margin: 7px;
+    background-color: blue;
   }
 </style>
 
 <section class="turns">
   <h1>Reserva tu turno</h1>
+  <div class="selectDay">
+    <div class="daysContainer">
+
+    </div>
+    <div class="daysContainer">
+
+    </div>
+    <div class="daysContainer">
+
+    </div>
+    <div class="daysContainer">
+
+    </div>
+    <div class="daysContainer">
+
+    </div>
+  </div>
+  <div class="selecttime">
+
+  </div>
+  <div class="formTurns">
+
+  </div>
 </section>
 <script type="text/javascript">
 
