78310dda67095fe2492016c5a1496144e25f227f
aelnajard <aelnajard@gmail.com>
2022-02-19 15:17:50 -0500

    ...

--- Pages/Index/Turns/Turns.php
+++ Pages/Index/Turns/Turns.php
@@ -1,1 +1,12 @@
-<h1>hola como estas</h1>
+<style media="screen">
+  .turns{
+    background-color: #0E7C7B;
+  }
+</style>
+
+<section class="turns">
+  <h1>Reserva tu turno</h1>
+</section>
+<script type="text/javascript">
+
+</script>
