bad3ab3b77346bb1c0cd205db840c813ce21814c
aelnajard <aelnajard@gmail.com>
2022-03-01 09:59:49 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -170,12 +170,12 @@ else if ($_POST['module'] == 'turnsSend'){
      $user->setPhoneNumber($_POST['PhoneUserTurn']);
      $user->setDate($_POST['date']);
 
-     $user->updateUser();
+    // $user->updateUser();
      echo $_POST['name']. " tu turno ha sido cambiado correctamente para la fecha: ". $_POST['date'];
   }
   elseif ($existUser == 0) {
      $user = new Users(new Database());
-     
+
      $user->setName($_POST['name']);
      $user->setEmail($_POST['email']);
      $user->setNIT($_POST['NITValue']);
