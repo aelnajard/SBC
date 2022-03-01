b73d09b13cbc8fd15b47765a68f69275a4d99c69
aelnajard <aelnajard@gmail.com>
2022-03-01 09:29:14 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -156,7 +156,9 @@ else if ($_POST['module'] == 'turnsSend'){
   $user = new Users(new Database());
   $user->setEmail($_POST['email']);
   $user->setNIT($_POST['NITValue']);
-echo json_encode  ($user->readUserExist()[0]["COUNT(*)"]);
+$var =  $user->readUserExist()["COUNT(*)"];
+
+echo $var;
 
 
 
