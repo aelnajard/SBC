67d01019dd4861c6181af986bf0045af2ae858c5
aelnajard <aelnajard@gmail.com>
2022-03-19 04:50:23 -0400

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -139,7 +139,7 @@ else if ($_POST['module'] == 'setAvatar') {
   $avatar->setAvatar($_POST['avatar']);
   $idAvatar = ($avatar->getAvatar()[0]->idAvatars);
 
-  //$idAvatar = readIdAvatar();
+  readIdAvatar();
 
   echo $idAvatar."Primer paso";
 
@@ -164,13 +164,13 @@ else if ($_POST['module'] == 'setAvatar') {
 
 }
 
-/*function readIdAvatar(){
-  $avatar = new Avatars(new Database());
+function readIdAvatar(){
+/*  $avatar = new Avatars(new Database());
   $avatar->setAvatar($_POST['avatar']);
   $idAvatar = ($avatar->getAvatar()[0]->idAvatars);
 
-  return $idAvatar;
-}*/
+  return $idAvatar;*/
+}
 
 else if ($_POST['module'] == 'getAvatar') {
   session_start();
